71242f3aee53b4fa078d6f545dbd8a2bbd04ff2a
afaz <abolfazlmajidi100@gmail.com>
2024-11-27 10:46:21 +0330

    add easy logging funcs

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/imafaz/logger
 
-go 1.23.3
+go 1.23
--- logger.go
+++ logger.go
@@ -1,62 +1,135 @@
 package logger
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"os"
 	"strings"
+	"sync"
 )
 
 const (
-	DEBUG = iota
+	_ = iota
+	DEBUG
 	INFO
 	WARN
 	ERROR
 	FATAL
 
-	CONSOLE_ONLY = iota
+	_ = iota
+	CONSOLE_ONLY
 	FILE_ONLY
 	CONSOLE_AND_FILE
 )
 
-func Init(logFileName string, outputOption int) {
-	var output io.Writer
+var (
+	outputOption int    = CONSOLE_ONLY
+	logFileName  string = "app.log"
+	logfile      *os.File
+	output       io.Writer
+	once         sync.Once
+)
 
-	logfile, err := os.OpenFile(logFileName, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
-	if err != nil {
-		Log(ERROR, "cant write log file: ", err.Error())
-		return
-	}
+func initLogFile() {
+	once.Do(func() {
+		var err error
+		logfile, err = os.OpenFile(logFileName, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+		if err != nil {
+			log.Fatal("can't write log file:", err.Error())
+		}
+	})
+}
 
+func SetOutput(option int) {
+	outputOption = option
 	switch outputOption {
 	case CONSOLE_ONLY:
 		output = os.Stdout
 	case FILE_ONLY:
+		initLogFile()
 		output = logfile
 	case CONSOLE_AND_FILE:
+		initLogFile()
 		output = io.MultiWriter(os.Stdout, logfile)
 	default:
-		Log(FATAL, "invalid output option")
+		logging(FATAL, "invalid output option")
 		return
 	}
-
 	log.SetOutput(output)
 }
+func SetLogFile(filePath string) {
 
-func Log(level int, messages ...string) {
+	var err error
+	logFileName = filePath
+	logfile, err = os.OpenFile(logFileName, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Println("can't write log file:", err.Error())
+		return
+	}
+	SetOutput(outputOption)
+}
+
+func logging(level int, messages ...string) {
 	message := strings.Join(messages, " ")
 	switch level {
 	case DEBUG:
-		log.Println("DEBUG:", message)
+		message = "DEBUG: " + message
 	case INFO:
-		log.Println("INFO:", message)
+		message = "INFO: " + message
 	case WARN:
-		log.Println("WARN:", message)
+		message = "WARN: " + message
 	case ERROR:
-		log.Println("ERROR:", message)
+		message = "ERROR: " + message
 	case FATAL:
-		log.Fatal("FATAL:", message)
-	default:
-		log.Fatal("ERROR:", "wrong log level!")
+		message = "FATAL: " + message
+		log.Fatalln(message)
 	}
+
+	log.Println(message)
+}
+
+func Debug(messages ...string) {
+	logging(DEBUG, messages...)
+}
+
+func Info(messages ...string) {
+	logging(INFO, messages...)
+}
+
+func Warn(messages ...string) {
+	logging(WARN, messages...)
+}
+
+func Error(messages ...string) {
+	logging(ERROR, messages...)
+}
+
+func Fatal(messages ...string) {
+	logging(FATAL, messages...)
+}
+
+func loggingf(level int, format string, args ...interface{}) {
+	message := fmt.Sprintf(format, args...)
+	logging(level, message)
+}
+
+func Debugf(format string, args ...interface{}) {
+	loggingf(DEBUG, format, args...)
+}
+
+func Infof(format string, args ...interface{}) {
+	loggingf(INFO, format, args...)
+}
+
+func Warnf(format string, args ...interface{}) {
+	loggingf(WARN, format, args...)
+}
+
+func Errorf(format string, args ...interface{}) {
+	loggingf(ERROR, format, args...)
+}
+
+func Fatalf(format string, args ...interface{}) {
+	loggingf(FATAL, format, args...)
 }
